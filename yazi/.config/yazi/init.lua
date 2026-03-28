68f42aedb18590662ffc145bb407dc17e3075c18
Gence Erdagi <gence@Gence.localdomain>
2026-03-28 18:00:39 +0300

    simple fix for multi file opening

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,5 +1,5 @@
 require("no-status"):setup()
-
+require("smart-enter"):setup({ open_multi = true })
 -- Yazi-ZX sidebar toggle support
 if os.getenv("YAZI_IDE_SIDEBAR") == "1" then
 	require("toggle-pane"):entry("min-parent")
