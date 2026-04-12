ce81f173718a6e127765bca05a1e7bd43644a9c3
GenceErdagi <70435995+GenceErdagi@users.noreply.github.com>
2026-04-12 21:39:49 +0300

    streamline ashen theme

--- helix/.config/helix/init.scm
+++ helix/.config/helix/init.scm
@@ -1,11 +1,8 @@
 (require (prefix-in helix. "helix/commands.scm"))
 (require "languages.scm")
-(require "themes/ashen.scm")
 (require "helix-server/helix-server.scm")
 (require "helix-file-opener/helix-file-opener.scm")
 (require "scooter/scooter.scm")
-
-(helix.theme "ashen")
 
 ;; Register handlers before starting server
 (register-handler "open" open-file-handler)
