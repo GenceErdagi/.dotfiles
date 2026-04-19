4a8f7b7b68e1870587f9b48c1bff4e1799ef68c5
GenceErdagi <70435995+GenceErdagi@users.noreply.github.com>
2026-04-19 18:13:55 +0300

    remove splash

--- helix/.config/helix/init.scm
+++ helix/.config/helix/init.scm
@@ -3,13 +3,8 @@
 (require "helix-server/helix-server.scm")
 (require "helix-file-opener/helix-file-opener.scm")
 (require "scooter/scooter.scm")
-(require "splash-hx/splash.scm")
 ;; Register handlers before starting server
 (register-handler "open" open-file-handler)
 
 (helix-server-start)
-(require "splash-hx/splash.scm")
 
-;; Add this to your init.scm
-(when (equal? (command-line) '("hx"))
-  (show-splash))
