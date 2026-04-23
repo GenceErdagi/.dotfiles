87547b9a34f826fcf121d76394d43a5caa157262
GenceErdagi <70435995+GenceErdagi@users.noreply.github.com>
2026-04-24 01:42:05 +0300

    Remove scooter references from config

--- helix/.config/helix/init.scm
+++ helix/.config/helix/init.scm
@@ -4,7 +4,6 @@
 
 (require "helix-server/helix-server.scm")
 (require "helix-file-opener/helix-file-opener.scm")
-(require "scooter/scooter.scm")
 ;; Register handlers before starting server
 (register-handler "open" open-file-handler)
 
