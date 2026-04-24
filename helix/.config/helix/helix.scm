eb9352662cf971f4a9579bf186033b942738e187
GenceErdagi <70435995+GenceErdagi@users.noreply.github.com>
2026-04-24 16:51:04 +0300

    focus-or-toggle added to helix

--- helix/.config/helix/helix.scm
+++ helix/.config/helix/helix.scm
@@ -1,7 +1,9 @@
 (require (prefix-in helix. "helix/commands.scm"))
 (require (prefix-in helix.static. "helix/static.scm"))
 (provide open-helix-scm
-         open-init-scm)
+         open-init-scm
+         sidebar-focus
+         terminal-focus)
 ;;@doc
 ;; Open the helix.scm file
 (define (open-helix-scm)
@@ -11,3 +13,14 @@
 ;; Opens the init.scm file
 (define (open-init-scm)
   (helix.open (helix.static.get-init-scm-path)))
+
+;;@doc
+;; Focuses on the zellij sidebar
+(define (sidebar-focus)
+  (helix.run-shell-command "zellij pipe --plugin zjide-manager --name focus-or-toggle-pane -- \"File-Explorer\"" ))
+
+
+;;@doc
+;; Focuses on the zellij sidebar
+(define (terminal-focus)
+  (helix.run-shell-command "zellij pipe --plugin zjide-manager --name focus-or-toggle-pane -- \"Terminal\"" ))
